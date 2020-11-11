c55932f70e831530134c896777b7963567a4c346
Oleh Serikov <oserikov@jerasoft.net>
2020-11-11 02:01:54 +0200

    Finish "Using Hooks in the Burger Builder" section

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
 
 import Aux from '../Aux/Aux';
@@ -6,37 +6,31 @@ import Tollbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
-class Layout extends Component {
-  state = {
-    showSideDrawer: false
+const Layout = props => {
+  const [sideDrawIsVisible, setSideDrawIsVisible] = useState(false);
+
+  const sideDrawerClosedHandler = () => {
+    setSideDrawIsVisible(false);
   }
 
-  sideDrawerClosedHandler = () => {
-    this.setState({showSideDrawer: false});
+  const sideDrawerToogleHandler = () => {
+    setSideDrawIsVisible(!sideDrawIsVisible);
   }
 
-  sideDrawerToogleHandler = () => {
-    this.setState((prevState) => {
-      return {showSideDrawer: !prevState.showSideDrawer};
-    });
-  }
-
-  render () {
-    return (
-      <Aux>
-        <Tollbar
-          isAuth={this.props.isAuthenticated}
-          drawerToogleClicked={this.sideDrawerToogleHandler} />
-        <SideDrawer
-          isAuth={this.props.isAuthenticated}
-          open={this.state.showSideDrawer} 
-          closed={this.sideDrawerClosedHandler} />
-        <main className={classes.Content}>
-          {this.props.children}
-        </main>
-      </Aux>
-    );
-  }
+  return (
+    <Aux>
+      <Tollbar
+        isAuth={props.isAuthenticated}
+        drawerToogleClicked={sideDrawerToogleHandler} />
+      <SideDrawer
+        isAuth={props.isAuthenticated}
+        open={sideDrawIsVisible}
+        closed={sideDrawerClosedHandler} />
+      <main className={classes.Content}>
+        {props.children}
+      </main>
+    </Aux>
+  );
 }
 
 const mapStateToProps = state => {
